5b658d0135805acccc5ff783a44a32b16ebc64c6
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2023-01-28 23:55:56 +0530

    added body parsed
    added body parsed

--- Form.js
+++ Form.js
@@ -1,19 +1,22 @@
-// const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const Form = express();
 
-Form.use((req, res, next) => {
-  console.log("sup");
-  next();
+Form.use(bodyParser.urlencoded({ extended: false }));
+
+Form.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form method="POST" action="/product"> <input type="text" name="title"></input><input type="number" name="size"></input><button type="submit">Add Product</button></form>'
+  );
+});
+Form.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-Form.use((req, res, next) => {
-  console.log("sup again");
-    res.send("<h1>Hello from expressJs</h1>");
-//   res.send({ key1: "value" });
+Form.use("/", (req, res, next) => {
+  res.send("<h1>Hello from expressJs</h1>");
 });
 
-// const server = http.createServer(app);
-// server.listen(3000);
 Form.listen(3000);
